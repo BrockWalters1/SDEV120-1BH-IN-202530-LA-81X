35a17656209abd2d495dc6b1a9a555e631b337e2
Brock Walters <113949557+BrockWalters1@users.noreply.github.com>
2026-02-22 14:20:01 -0500

    Add files via upload

---
+++ Module 6 Assignment (Brock Walters).py
@@ -0,0 +1,9 @@
+# Variables for the assignment
+c = "cats"
+d = "dogs"
+
+# Create the sentence string
+s = "It is raining " + c + " and " + d + "."
+
+# Final command to display the output
+print(s)
